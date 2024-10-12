227fa4117a776060ec8d7300da9b95ac079b4091
davidsponseller <161887842+davidsponseller@users.noreply.github.com>
2024-10-12 10:07:54 -0600

    attempt to find bug in AI

--- probe-simulation/src/scripts/ai.py
+++ probe-simulation/src/scripts/ai.py
@@ -21,35 +21,38 @@ def behaviors(ai):
 
 
 def droneFire(target, origin, lastFire):
-    if abs(monotonic() - lastFire) > 3:
+    if abs(int(abs(monotonic())) - int(abs(lastFire))) > 3:
         lasers.fire(origin=origin, target=target, destroy=False)
-        print(lastFire)
-        print(monotonic())
-        lastFire = monotonic()
+        print(abs(int(abs(monotonic())) - int(abs(lastFire))))
+        lastFire = int(abs(monotonic()))
 
 
 def removeChar(ai, ship):
     behaviors(ai["ai"]).FLEE(ship, 100000, 100000, 0)
 
+
 updateTime = 0
+
+
 def update(AIworld, aiChars, ship):
     global updateTime
-    updateTime+=1
-    if updateTime==8:
-        updateTime =0
+    updateTime += 1
+    if updateTime == 8:
+        updateTime = 0
         for aiChar in aiChars:
+            char = aiChars[aiChar]
             ai = aiChars[aiChar]["ai"]
             node = aiChars[aiChar]["mesh"]
-            if aiChars[aiChar]["active"]:
+            if char["active"]:
                 if ship.getDistance(node) > 50:
                     behaviors(ai).PURSUE(ship)
                 else:
-                    behaviors(ai).REMOVE('pursue')
+                    behaviors(ai).REMOVE("pursue")
                     node.lookAt((ship.get_x(), ship.get_y(), ship.get_z()))
-                    node.setP(node.getP() +180)
-                    node.setR(node.getR() +180)
+                    node.setP(node.getP() + 180)
+                    node.setR(node.getR() + 180)
                     if ship.getDistance(node) <= 50:
-                        droneFire(ship, node, aiChars[aiChar]['lastFire'])
+                        droneFire(ship, node, char["lastFire"])
             else:
                 behaviors(ai).FLEE(ship, 10000, 10000, 1)
     AIworld.update()
